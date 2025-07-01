e0c967511f18ea440a3d2d008a9605596cb116e0
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-01 11:55:16 -0400

    Update power.m
    Rework power method to match optimized loop format from other methods.

--- +factors/@integerFactors/power.m
+++ +factors/@integerFactors/power.m
@@ -17,18 +17,20 @@ function C = power(A,B)
                 C = factors.integerFactors.ones(size(A));
                 C.IsZero(A.IsZero) = true;
             end
-            C = A;
-            C(B.IsZero) = factors.integerFactors.ones(size(C(B.IsZero)));
-            absB = uint8(abs(B));
-            C.IsNegative(A.IsNegative & mod(absB,2)==0) = false;
-            elementIndices = find(~(B.IsZero(:) | A.IsZero(:))).';
-            for elementIndex = elementIndices
-                if any(A.Exponents{elementIndex}>idivide(intmax("uint8"),absB(elementIndex)))
-                    errorID = "integerFactors:power:exponentSumExceedsIntMax";
-                    message = "Product of factor exponents and integer power exceeds the largest value of type uint8. See INTMAX.";
-                    error(errorID,message)
+            isNontrivial = ~(A.IsZero | B.IsZero);
+            if any(isNontrivial,"all")
+                C(isNontrivial) = A(isNontrivial);
+                absB = uint8(abs(B(isNontrivial)));
+                CIndices = find(isNontrivial);
+                for elementIndex = 1:numel(absB)
+                    if any(A.Exponents{CIndices(elementIndex)}>idivide(intmax("uint8"),absB(elementIndex)))
+                        errorID = "integerFactors:power:exponentSumExceedsIntMax";
+                        message = "Product of factor exponents and integer power exceeds the largest value of type uint8. See INTMAX.";
+                        error(errorID,message)
+                    end
+                    C.Exponents{CIndices(elementIndex)} = C.Exponents{CIndices(elementIndex)}.*absB(elementIndex);
                 end
-                C.Exponents{elementIndex} = C.Exponents{elementIndex}.*absB(elementIndex);
+                C.IsNegative(CIndices(A.IsNegative(isNontrivial) & mod(absB,2)==0)) = false;
             end
         end
     end
