7ef8e83a2ef22bf3c9dd3caa906434ca7cbc766d
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-02 20:17:08 -0400

    Update cast.m
    Update argument validation to allow for "like" casting. Add comments throughout.

--- +factors/@integerFactors/cast.m
+++ +factors/@integerFactors/cast.m
@@ -1,8 +1,24 @@
-function B = cast(A,newclass)
-    arguments
-        A
-        newclass    {mustBeTextScalar}
+function B = cast(A,varargin)
+    % Perform input argument validation
+    narginchk(2,3)
+    try
+        mustBeTextScalar(varargin{1})
+    catch ME
+        errorID = "integerFactors:validation:mustBeTextScalar";
+        message = strcat("Invalid argument at position 2. ",ME.message);
+        error(errorID,message)
     end
+    if nargin==2
+        newclass = varargin{1};
+    else
+        if ~strcmp(varargin{1},"like")
+            errorID = "integerFactors:cast:invalidString";
+            message = "With three input arguments, the second argument must be 'like'.";
+            error(errorID,message)
+        end
+        newclass = class(varargin{2});
+    end
+    % Check for valid new class
     floatClassnames     = ["double";...
                            "single"];
     signedClassnames    = ["int8";...
@@ -21,47 +37,61 @@ function B = cast(A,newclass)
         message = "Unsupported data conversion from 'factors.integerFactors' to '%s'.";
         error(errorID,message,newclass)
     end
-    if isUnsignedClassname && any(A.IsNegative,"all")
-        errorID = "integerFactors:cast:typeDoesNotSupportNegativeNumbers";
-        message = "Unsigned integer types cannot represent negative numbers.";
-        error(errorID,message)
+    if isUnsignedClassname
+        try
+            mustBeNonnegative(A)
+        catch
+            errorID = "integerFactors:cast:typeDoesNotSupportNegativeNumbers";
+            message = "Unsigned integer types cannot represent negative numbers.";
+            error(errorID,message)
+        end
     end
+    % Initialize the output array
+    B = zeros(size(A),newclass);
+    % Find the maximum integer for lossless conversion
     if isFloatClassname
         maxInteger = flintmax(newclass);
     else
         maxInteger = intmax(newclass);
     end
-    B = zeros(size(A),newclass);
-    elementIndices = find(~A.IsZero(:)).';
-    for elementIndex = elementIndices
-        fixedQuotient = intmax("uint64");
-        for factorIndex = length(A.Factors{elementIndex}):-1:1
-            factor = uint64(A.Factors{elementIndex}(factorIndex));
-            for count = 1:A.Exponents{elementIndex}(factorIndex)
-                fixedQuotient = idivide(fixedQuotient,factor);
-                if fixedQuotient==0
-                    errorID = "integerFactors:cast:exceedsIntMax";
-                    message = "Product of factors exceeds the largest value of type uint64. See INTMAX.";
+    % Check for nonzero elements
+    isNonzero = ~A.IsZero;
+    if any(isNonzero,"all")
+        arrayIndices = find(~A.IsZero(:));
+        for elementIndex = 1:numel(arrayIndices)
+            % Check for lossless conversion to type "uint64"
+            fixedQuotient = intmax("uint64");
+            for factorIndex = length(A.Factors{arrayIndices(elementIndex)}):-1:1
+                factor = uint64(A.Factors{arrayIndices(elementIndex)}(factorIndex));
+                for count = 1:A.Exponents{arrayIndices(elementIndex)}(factorIndex)
+                    fixedQuotient = idivide(fixedQuotient,factor);
+                    if fixedQuotient==0
+                        errorID = "integerFactors:cast:exceedsIntMax";
+                        message = "Product of factors exceeds the largest value of type uint64. See INTMAX.";
+                        error(errorID,message)
+                    end
+                end
+            end
+            % Perform conversion to type "uint64"
+            absInteger = prod(uint64(A.Factors{arrayIndices(elementIndex)}).^uint64(A.Exponents{arrayIndices(elementIndex)}));
+            % Check for lossless conversion to requested type
+            if absInteger>maxInteger
+                if isFloatClassname
+                    errorID = "integerFactors:cast:exceedsFloatIntMax";
+                    message = "Product of factors exceeds the largest consecutive integer in floating-point format. See FLINTMAX.";
+                    error(errorID,message)
+                else
+                    errorID = "integerFactors:cast:exceedsSpecifiedIntMax";
+                    message = "Product of factors exceeds the largest value of the specified integer type. See INTMAX.";
                     error(errorID,message)
                 end
             end
-        end
-        absInteger = prod(uint64(A.Factors{elementIndex}).^uint64(A.Exponents{elementIndex}));
-        if absInteger>maxInteger
-            if isFloatClassname
-                errorID = "integerFactors:cast:exceedsFloatIntMax";
-                message = "Product of factors exceeds the largest consecutive integer in floating-point format. See FLINTMAX.";
-                error(errorID,message)
+            % Assign value to output array
+            if A.IsNegative(arrayIndices(elementIndex))
+                B(arrayIndices(elementIndex)) = -absInteger;
             else
-                errorID = "integerFactors:cast:exceedsSpecifiedIntMax";
-                message = "Product of factors exceeds the largest value of the specified integer type. See INTMAX.";
-                error(errorID,message)
+                B(arrayIndices(elementIndex)) = absInteger;
             end
-        end
-        if A.IsNegative(elementIndex)
-            B(elementIndex) = -absInteger;
-        else
-            B(elementIndex) = absInteger;
         end
     end
 end
