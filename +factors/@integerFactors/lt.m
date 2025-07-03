73aa1cdaf52bfc7e2917812a306353676816ca67
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-03 17:44:21 -0400

    Update lt.m
    Add comments throughout, minor optimization

--- +factors/@integerFactors/lt.m
+++ +factors/@integerFactors/lt.m
@@ -14,12 +14,17 @@ function TF = lt(A,B)
 %       scalars | vectors | matrices | multidimensional arrays
 %
 %   See also eq, ge, gt, le, ne
+
+    % Implicitly expand singleton dimensions of input arrays
     [A,B] = utility.implicitArrayExpansion(A,B);
     if isempty(A)
+        % Return empty array
         TF = logical.empty(size(A));
     else
-        TF = ~isnan(A) & ~isnan(B);
+        % Return false if A or B are NaN
+        TF = ~(isnan(A) | isnan(B));
         if any(TF,"all")
+            % Compare elements where A and B are finite or infinite
             TF(TF) = ~(A(TF)>=B(TF));
         end
     end
