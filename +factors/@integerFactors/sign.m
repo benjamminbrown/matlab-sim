7c917820f1ce0fd6693a2ab29b377671957fe0f0
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-03 19:54:41 -0400

    Update sign.m
    Add help documentation

--- +factors/@integerFactors/sign.m
+++ +factors/@integerFactors/sign.m
@@ -1,4 +1,17 @@
 function X = sign(X)
+% SIGN - Sign function (signum function)
+%   This function returns a factors.integerFactors array that is zero where
+%   the input array is zero, +1 where the input array is positive, and -1
+%   where the input array is negative.
+%
+%   Syntax
+%     SIGN(X)
+%
+%   Input Arguments
+%     X - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also abs
     X.Factors(:) = {uint64.empty(1,0)};
     X.Exponents(:) = {uint8.empty(1,0)};
 end
