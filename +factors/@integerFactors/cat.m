4832c56faba814ce87c513356dc3a06ce3dbd0ff
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-03 08:31:14 -0400

    Update cat.m
    Add help documentation

--- +factors/@integerFactors/cat.m
+++ +factors/@integerFactors/cat.m
@@ -1,4 +1,18 @@
 function obj = cat(dim,varargin)
+% CAT - Concatenate arrays
+%   This function concatenates arrays with compatible sizes sequentially
+%   along the specified dimension.
+%
+%   Syntax
+%     C = CAT(dim,A1,A2,...,AN)
+%
+%   Input Arguments
+%     dim - Dimension to operate along
+%       positive integer scalar
+%     A1,A2,...,AN - List of input arrays
+%       scalars | vectors | matrices | multidimensional arrays
+%
+%   See also vertcat, horzcat
     arguments
         dim
     end
