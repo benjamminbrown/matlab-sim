0e870c6eef07b28e3d969af222e8c60af9fdb5cf
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-02 20:02:00 -0400

    Update abs.m
    Add help documentation

--- +factors/@integerFactors/abs.m
+++ +factors/@integerFactors/abs.m
@@ -1,4 +1,16 @@
 function Y = abs(X)
+% ABS - Absolute value
+%   This function returns the absolute value of each element in the input
+%   array.
+%
+%   Syntax
+%     Y = ABS(X)
+%
+%   Input Arguments
+%     X - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also sign
     Y = X;
     Y.IsNegative(:) = false;
 end
