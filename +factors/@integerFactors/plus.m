4b76fe10bb56aeccfcdeba4e91d43c163609dbd6
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-04 10:42:02 -0400

    Update plus.m
    Add help documentation, optimizations, and comments throughout

--- +factors/@integerFactors/plus.m
+++ +factors/@integerFactors/plus.m
@@ -1,9 +1,25 @@
 function C = plus(A,B)
+% PLUS - Add numbers, append strings
+%   This function adds the corresponding elements of the input arrays.
+%
+%   Syntax
+%     C = A + B
+%     C = PLUS(A,B)
+%
+%   Input Arguments
+%     A - Operands
+%       scalars | vectors | matrices | multidimensional arrays
+%     B - Operands
+%       scalars | vectors | matrices | multidimensional arrays
+%
+%   See also minus, uplus
     arguments
         A   factors.integerFactors
         B   factors.integerFactors
     end
+    % Extract any common factors and perform addition on reduced arrays
     [R,Ar,Br] = commonFactors(A,B);
+    % Initialize the sum with a trivial array
     isAr = ~Ar.IsZero & Br.IsZero;
     isBr = Ar.IsZero & ~Br.IsZero;
     isZero = Ar==-Br;
@@ -22,34 +38,43 @@ function C = plus(A,B)
         Cr(isAr) = Ar(isAr);
         Cr(isBr) = Br(isBr);
     end
+    % Find where any nontrivial calculation will be required
     isNontrivial = ~Ar.IsZero & ~Br.IsZero & Ar~=-Br;
     if any(isNontrivial,"all")
+        % Perform nontrivial calculation where appropriate
         absAr = uint64(abs(Ar(isNontrivial)));
         absBr = uint64(abs(Br(isNontrivial)));
         absCr = zeros(size(absAr),"uint64");
+        % Perform addition of absolute values where appropriate
         isAddition = Ar.IsNegative(isNontrivial)==Br.IsNegative(isNontrivial);
         if any(absAr(isAddition)>intmax("uint64")-absBr(isAddition))
+            % Throw an error if the sum exceeds the maximal integer value
             errorID = "integerFactor:plus:exponentSumExceedsIntMax";
-            message = "Sum of reduced operands exceeds the largest value of type uint64. See INTMAX.";
+            message = "Sum of reduced operands exceeds the largest value of type 'uint64'. See INTMAX.";
             error(errorID,message)
         end
         absCr(isAddition) = absAr(isAddition)+absBr(isAddition);
+        % Perform subtraction of absolute values where appropriate
         isSubtraction = ~isAddition;
         isGreaterAbsAr = absAr>absBr;
         isSubtractionWithGreaterAbsAr = isSubtraction & isGreaterAbsAr;
-        absCr(isSubtractionWithGreaterAbsAr) = absAr(isSubtractionWithGreaterAbsAr)-absBr(isSubtractionWithGreaterAbsAr);
-        isSubtractionWithGreaterAbsBr = isSubtraction & ~isGreaterAbsAr;
-        absCr(isSubtractionWithGreaterAbsBr) = absBr(isSubtractionWithGreaterAbsBr)-absAr(isSubtractionWithGreaterAbsBr);
-        Cr(isNontrivial) = factors.integerFactors(absCr);
-        CrIndices = find(isNontrivial);
-        isNegativeAr = isSubtractionWithGreaterAbsAr & Ar.IsNegative(CrIndices);
-        if any(isNegativeAr)
-            Cr(CrIndices(isNegativeAr)) = -Cr(CrIndices(isNegativeAr));
+        if any(isSubtractionWithGreaterAbsAr)
+            absCr(isSubtractionWithGreaterAbsAr) = absAr(isSubtractionWithGreaterAbsAr)-absBr(isSubtractionWithGreaterAbsAr);
         end
-        isNegativeBr = isSubtractionWithGreaterAbsBr & Br.IsNegative(CrIndices);
-        if any(isNegativeBr)
-            Cr(CrIndices(isNegativeBr)) = -Cr(CrIndices(isNegativeBr));
+        isSubtractionWithGreaterAbsBr = isSubtraction & ~isGreaterAbsAr;
+        if any(isSubtractionWithGreaterAbsBr)
+            absCr(isSubtractionWithGreaterAbsBr) = absBr(isSubtractionWithGreaterAbsBr)-absAr(isSubtractionWithGreaterAbsBr);
+        end
+        % Perform prime factorization on the nontrivial results
+        Cr(isNontrivial) = factors.integerFactors(absCr);
+        % Negate the signs of the nontrivial results where appropriate
+        CrIndices = find(isNontrivial);
+        isNegativeCr = (isSubtractionWithGreaterAbsAr & Ar.IsNegative(CrIndices)) | ...
+                       (isSubtractionWithGreaterAbsBr & Br.IsNegative(CrIndices));
+        if any(isNegativeCr)
+            Cr(CrIndices(isNegativeCr)) = -Cr(CrIndices(isNegativeCr));
         end
     end
+    % Multiply by common factors
     C = R.*Cr;
 end
