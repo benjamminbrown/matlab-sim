264fae2b56333d5cde4245eb8c0f792dc4b163fd
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-04 09:13:28 -0400

    Modify mod and rem methods
    Add help documentation

--- +factors/@integerFactors/mod.m
+++ +factors/@integerFactors/mod.m
@@ -1,4 +1,18 @@
 function B = mod(A,M)
+% MOD - Remainder after division (modulo operation)
+%   This function returns the remainder after division of a specified
+%   dividend by a specified divisor.
+%
+%   Syntax
+%     B = MOD(A,M)
+%
+%   Input Arguments
+%     A - Dividend
+%       scalar | vector | matrix | multidimensional array
+%     M - Divisor
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also rem
     arguments
         A   factors.integerFactors
         M   factors.integerFactors
