edb69ea528a9925b35a8a8d1abb27ea0151114fd
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-04 09:23:16 -0400

    Update unary plus and minus methods
    Add help documentation and modify function signature to match documentation

--- +factors/@integerFactors/uminus.m
+++ +factors/@integerFactors/uminus.m
@@ -1,3 +1,15 @@
-function X = uminus(X)
-    X.IsNegative = ~X.IsNegative;
+function A = uminus(A)
+% UMINUS - Unary minus
+%   This function negates the elements of A and stores the result in C.
+%
+%   Syntax
+%     C = +A
+%     C = UMINUS(A)
+%
+%   Input Arguments
+%     A - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also uplus, minus
+    A.IsNegative = ~A.IsNegative;
 end
