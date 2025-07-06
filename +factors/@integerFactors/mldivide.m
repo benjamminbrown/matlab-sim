305d8c2e3ee2e28dda14cf202b8a6e4b10bd779d
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-06 16:24:00 -0400

    Add matrix division methods

---
+++ +factors/@integerFactors/mldivide.m
@@ -0,0 +1,27 @@
+function C = mldivide(A,B)
+% MLDIVIDE - Solve systems of linear equations Ax = B for x
+%   This function solves the system of linear equations A*x = B for x. For
+%   factors.integerFactors operands, this overloaded function will only
+%   work if at least one of the operands is a scalar.
+%
+%   Syntax
+%     x = A\B
+%     x = MLDIVIDE(A,B)
+%
+%   Input Arguments
+%     A - Left operand
+%       scalar | vector | matrix | multidimensional array
+%     B - Right operand
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also mrdivide, ldivide, rdivide
+    if ~(isscalar(A) || isscalar(B))
+        % Throw error if neither input is a scalar
+        errorID = "integerFactors:mldivide:matrixDivisionNotSupported";
+        message = "The matrix divide method is not supported for two nonscalar operands of type 'factors.integerFactors'.";
+        error(errorID,message)
+    else
+        % Pass operands to ldivide method
+        C = ldivide(A,B);
+    end
+end
---
+++ +factors/@integerFactors/mrdivide.m
@@ -0,0 +1,27 @@
+function C = mrdivide(B,A)
+% MRDIVIDE - Solve systems of linear equations xA = B for x
+%   This function solves the system of linear equations x*A = B for x. For
+%   factors.integerFactors operands, this overloaded function will only
+%   work if at least one of the operands is a scalar.
+%
+%   Syntax
+%     x = B/A
+%     x = MRDIVIDE(B,A)
+%
+%   Input Arguments
+%     B - Left operand
+%       scalar | vector | matrix | multidimensional array
+%     A - Right operand
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also mldivide, rdivide, ldivide
+    if ~(isscalar(A) || isscalar(B))
+        % Throw error if neither input is a scalar
+        errorID = "integerFactors:mrdivide:matrixDivisionNotSupported";
+        message = "The matrix divide method is not supported for two nonscalar operands of type 'factors.integerFactors'.";
+        error(errorID,message)
+    else
+        % Pass operands to rdivide method
+        C = rdivide(B,A);
+    end
+end
