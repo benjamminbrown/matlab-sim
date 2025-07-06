fe385f00f00bfd80b6aba25a353a0c6f64d989e1
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-06 15:11:21 -0400

    Update mtimes.m
    Add help documentation

--- +factors/@integerFactors/mtimes.m
+++ +factors/@integerFactors/mtimes.m
@@ -1,4 +1,20 @@
 function C = mtimes(A,B)
+% MTIMES - Matrix multiplication
+%   This function returns the matrix product of the input arrays. For
+%   factors.integerFactors arrays, this overloaded function will only work
+%   if at least one of the operands is a scalar.
+%
+%   Syntax
+%     C = A*B
+%     C = MTIMES(A,B)
+%
+%   Input Arguments
+%     A - Left operand
+%       scalar | vector | matrix | multidimensional array
+%     B - Right operand
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also times
     arguments
         A   factors.integerFactors
         B   factors.integerFactors
