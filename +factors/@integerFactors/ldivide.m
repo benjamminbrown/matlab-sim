69b70fe53cb28d6249303e923f1ad18470813e0b
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-06 15:47:34 -0400

    Update division methods
    Add help documentation

--- +factors/@integerFactors/ldivide.m
+++ +factors/@integerFactors/ldivide.m
@@ -1,3 +1,22 @@
 function C = ldivide(A,B)
+% LDIVIDE - Left array division
+%   This function divides each element of the right array by the
+%   corresponding element of the left array. For factors.integerFactors
+%   arrays, this overloaded function is equivalent to constructing an
+%   appropriate factors.rationalFactors array.
+%
+%   Syntax
+%     x = A./B
+%     x = LDIVIDE(A,B)
+%
+%   Input Arguments
+%     A - Left operand
+%       scalar | vector | matrix | multidimensional array
+%     B - Right operand
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also rdivide, mldivide, mrdivide
+
+    % Pass operands to the factors.rationalFactors constructor
     C = factors.rationalFactors(B,A);
 end
--- +factors/@integerFactors/rdivide.m
+++ +factors/@integerFactors/rdivide.m
@@ -1,3 +1,22 @@
 function C = rdivide(A,B)
+% RDIVIDE - Right array division
+%   This function divides each element of the left array by the
+%   corresponding element of the right array. For factors.integerFactors
+%   arrays, this overloaded function is equivalent to constructing an
+%   appropriate factors.rationalFactors array.
+%
+%   Syntax
+%     x = A./B
+%     x = RDIVIDE(A,B)
+%
+%   Input Arguments
+%     A - Left operand
+%       scalar | vector | matrix | multidimensional array
+%     B - Right operand
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also ldivide, mrdivide, mldivide
+
+    % Pass operands to the factors.rationalFactors constructor
     C = factors.rationalFactors(A,B);
 end
