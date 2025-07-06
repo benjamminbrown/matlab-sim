9db1519df842c77ba5d8762af04776b22adfa382
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-06 16:23:10 -0400

    Update ldivide and function signatures
    Revise help documentation and pass to rdivide method instead of rationalFactors constructor

--- +factors/@integerFactors/ldivide.m
+++ +factors/@integerFactors/ldivide.m
@@ -1,4 +1,4 @@
-function C = ldivide(A,B)
+function C = ldivide(B,A)
 % LDIVIDE - Left array division
 %   This function divides each element of the right array by the
 %   corresponding element of the left array. For factors.integerFactors
@@ -6,17 +6,17 @@ function C = ldivide(A,B)
 %   appropriate factors.rationalFactors array.
 %
 %   Syntax
-%     x = A./B
-%     x = LDIVIDE(A,B)
+%     x = B.\A
+%     x = LDIVIDE(B,A)
 %
 %   Input Arguments
-%     A - Left operand
+%     B - Left operand
 %       scalar | vector | matrix | multidimensional array
-%     B - Right operand
+%     A - Right operand
 %       scalar | vector | matrix | multidimensional array
 %
 %   See also rdivide, mldivide, mrdivide
 
-    % Pass operands to the factors.rationalFactors constructor
-    C = factors.rationalFactors(B,A);
+    % Pass operands to the rdivide method
+    C = rdivide(A,B);
 end
