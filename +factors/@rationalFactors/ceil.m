529bfa583bad0c80013024de0b24351634143015
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-07 17:22:15 -0400

    Update ceil.m
    Add help documentation and comments

--- +factors/@rationalFactors/ceil.m
+++ +factors/@rationalFactors/ceil.m
@@ -1,7 +1,21 @@
 function Y = ceil(X)
+% CEIL - Round toward positive infinity
+%   This function rounds each element of the input array to the nearest
+%   integer greater than or equal to that element.
+%
+%   Syntax
+%     Y = CEIL(X)
+%
+%   Input Arguments
+%     X - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also fix, floor, round
     Y = fix(X);
+    % Check for finite positive nonintegers
     isFinitePositiveNoninteger = isfinite(X) & X.Numerator.IsNegative==X.Denominator.IsNegative & X~=Y;
     if any(isFinitePositiveNoninteger,"all")
+        % Modify the result where appropriate
         Y(isFinitePositiveNoninteger) = Y(isFinitePositiveNoninteger)+1;
     end
 end
