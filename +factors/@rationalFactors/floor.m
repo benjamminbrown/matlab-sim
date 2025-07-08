6fb1adbcf9b1401419c91209fe2b5e3f1b40112d
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-07 20:31:58 -0400

    Update floor.m
    Add help documentation and comments

--- +factors/@rationalFactors/floor.m
+++ +factors/@rationalFactors/floor.m
@@ -1,7 +1,21 @@
 function Y = floor(X)
+% FLOOR - Round toward negative infinity
+%   This function rounds each element of the input array to the nearest
+%   integer less than or equal to that element.
+%
+%   Syntax
+%     Y = FLOOR(X)
+%
+%   Input Arguments
+%     X - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also ceil, fix, round
     Y = fix(X);
+    % Check for finite negative nonintegers
     isFiniteNegativeNoninteger = isfinite(X) & X.Numerator.IsNegative~=X.Denominator.IsNegative & X~=Y;
     if any(isFiniteNegativeNoninteger,"all")
+        % Modify the result where appropriate
         Y(isFiniteNegativeNoninteger) = Y(isFiniteNegativeNoninteger)-1;
     end
 end
