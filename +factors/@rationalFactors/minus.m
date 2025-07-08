36d4e41275668affa4fd791197c73bcd3230c3b5
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-07 21:03:54 -0400

    Update binary operators
    Add help documentation

--- +factors/@rationalFactors/minus.m
+++ +factors/@rationalFactors/minus.m
@@ -1,3 +1,18 @@
 function C = minus(A,B)
+% MINUS - Subtraction
+%   This function subtracts the corresponding elements of the right array
+%   from those of the left array.
+%
+%   Syntax
+%     C = A - B
+%     C = MINUS(A,B)
+%
+%   Input Arguments
+%     A - Left operand
+%       scalar | vector | matrix | multidimensional array
+%     B - Right operand
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also plus, uminus
     C = A+-B;
 end
