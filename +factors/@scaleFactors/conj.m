76801143edcea9d7c600a469a83cf360a14c9e29
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-08 16:44:26 -0400

    Add conj method and function signature

---
+++ +factors/@scaleFactors/conj.m
@@ -0,0 +1,15 @@
+function Z = conj(Z)
+% conj - Complex conjugate
+%   This function returns an array of the complex conjugates of each
+%   element in the input array. Since all factors.scaleFactors arrays are
+%   strictly real, this function returns the unmodified input array.
+%
+%   Syntax
+%     Zc = conj(Z)
+%
+%   Input Arguments
+%     Z - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also i, j, imag, real, ctranspose, complex
+end
