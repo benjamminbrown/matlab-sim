d4b5bdf5cd9e5fdc02d03b81ca943d7735e1d8e0
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-07 20:56:43 -0400

    Update abs.m
    Add help documentation

--- +factors/@rationalFactors/abs.m
+++ +factors/@rationalFactors/abs.m
@@ -1,4 +1,16 @@
 function X = abs(X)
+% ABS - Absolute value
+%   This function returns the absolute value of each element in the input
+%   array.
+%
+%   Syntax
+%     Y = ABS(X)
+%
+%   Input Arguments
+%     X - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also sign
     X.Numerator = abs(X.Numerator);
     X.Denominator = abs(X.Denominator);
 end
