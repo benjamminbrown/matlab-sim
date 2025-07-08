50e844e1694e63ea85d0ce2566951f3e379c4046
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-07 21:01:21 -0400

    Update unary operators
    Add help documentation

--- +factors/@rationalFactors/uminus.m
+++ +factors/@rationalFactors/uminus.m
@@ -1,3 +1,15 @@
 function X = uminus(X)
+% UMINUS - Unary minus
+%   This function returns the input array with its elements negated.
+%
+%   Syntax
+%     C = -A
+%     C = UMINUS(A)
+%
+%   Input Arguments
+%     A - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also uplus, minus
     X.Numerator = -X.Numerator;
 end
