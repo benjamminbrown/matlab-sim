99cb58ecbc23d079b0c62f3d705412bb5c12a8e8
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-07 20:55:42 -0400

    Update mod and rem methods
    Add help documentation

--- +factors/@rationalFactors/mod.m
+++ +factors/@rationalFactors/mod.m
@@ -1,4 +1,18 @@
 function B = mod(A,M)
+% MOD - Remainder after division (modulo operation)
+%   This function returns the remainder after division of a specified
+%   dividend by a specified divisor.
+%
+%   Syntax
+%     B = MOD(A,M)
+%
+%   Input Arguments
+%     A - Dividend
+%       scalar | vector | matrix | multidimensional array
+%     M - Divisor
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also rem
     arguments
         A   factors.rationalFactors
         M   factors.rationalFactors
@@ -12,6 +26,6 @@ function B = mod(A,M)
     isValidMod = isfinite(A) & ~isZeroM;
     if any(isValidMod,"all")
         B(isValidMod) = factors.rationalFactors(mod(A.Numerator(isValidMod).*M.Denominator(isValidMod),M.Numerator(isValidMod).*A.Denominator(isValidMod)), ...
-                                    A.Denominator(isValidMod).*M.Denominator(isValidMod));
+                                                A.Denominator(isValidMod).*M.Denominator(isValidMod));
     end
 end
