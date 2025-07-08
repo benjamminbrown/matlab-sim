8a759de71b074cb3662fe0bd4cfbe9a98f98c75a
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-07 20:57:49 -0400

    Update permute.m
    Add help documentation

--- +factors/@rationalFactors/permute.m
+++ +factors/@rationalFactors/permute.m
@@ -1,4 +1,18 @@
 function A = permute(A,dimorder)
+% PERMUTE - Permute array dimensions
+%   This function rearranges the dimensions of an array in the specified
+%   order.
+%
+%   Syntax
+%     B = PERMUTE(A,dimorder)
+%
+%   Input Arguments
+%     A - Input array
+%       scalar | vector | matrix | multidimensional array
+%     dimorder - Dimension order
+%       positive integer row vector
+%
+%   See also ipermute, reshape, shiftdim, transpose
     A.Numerator = permute(A.Numerator,dimorder);
     A.Denominator = permute(A.Denominator,dimorder);
 end
