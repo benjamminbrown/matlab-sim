10d6d44128e29408a5349f5f89e115ada90fd1e3
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-07 20:45:27 -0400

    Update is* methods
    Add help documentation

--- +factors/@rationalFactors/isfinite.m
+++ +factors/@rationalFactors/isfinite.m
@@ -1,3 +1,15 @@
 function TF = isfinite(A)
+% ISFINITE - Determine which array elements are finite
+%   This function returns a logical array indicating where the input array
+%   is finite (neither infinite nor NaN).
+%
+%   Syntax
+%     TF = ISFINITE(A)
+%
+%   Input Arguments
+%     A - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also isinf, isnan
     TF = ~A.Denominator.IsZero;
 end
--- +factors/@rationalFactors/isinf.m
+++ +factors/@rationalFactors/isinf.m
@@ -1,3 +1,15 @@
 function TF = isinf(A)
+% ISINF - Determine which array elements are infinite
+%   This function returns a logical array indicating where the input array
+%   is infinite (neither finite nor NaN).
+%
+%   Syntax
+%     TF = ISINF(A)
+%
+%   Input Arguments
+%     A - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also isfinite, isnan
     TF = A.Denominator.IsZero & ~A.Numerator.IsZero;
 end
