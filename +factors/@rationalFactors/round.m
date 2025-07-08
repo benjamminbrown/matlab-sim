7b93f7f00a7c655ef8d4b5aee4963e92c6ccbd4a
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-07 20:32:09 -0400

    Update round.m
    Add help documentation

--- +factors/@rationalFactors/round.m
+++ +factors/@rationalFactors/round.m
@@ -1,4 +1,16 @@
 function X = round(X)
+% ROUND - Round to nearest integer
+%   This function rounds each element of the input array to the nearest
+%   integer.
+%
+%   Syntax
+%     Y = ROUND(X)
+%
+%   Input Arguments
+%     X - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also ceil, fix, floor
     shiftedX = X+factors.rationalFactors(1,2);
     X = shiftedX-mod(shiftedX,1);
 end
