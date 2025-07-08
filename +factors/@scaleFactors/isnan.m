3e582d80b7ccad6751ea25ff299845bbb7e35ed4
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-08 16:28:39 -0400

    Update is* methods
    Add help documentation

--- +factors/@scaleFactors/isnan.m
+++ +factors/@scaleFactors/isnan.m
@@ -1,3 +1,16 @@
 function TF = isnan(A)
+% ISNAN - Determine which array elements are NaN
+%   This function returns a logical array indicating where the input array
+%   is NaN (neither finite nor infinite). This overloaded function will
+%   always return false arrays for factors.scaleFactors arrays.
+%
+%   Syntax
+%     TF = ISNAN(A)
+%
+%   Input Arguments
+%     A - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also isfinite, isinf
     TF = false(size(A));
 end
--- +factors/@scaleFactors/isreal.m
+++ +factors/@scaleFactors/isreal.m
@@ -1,3 +1,16 @@
 function TF = isreal(~)
+% ISREAL - Determine whether array uses complex storage
+%   This function returns a logical scalar indicating whether the input
+%   array does not have an imaginary part. This overloaded function will
+%   always return true for factors.scaleFactors arrays.
+%
+%   Syntax
+%     TF = ISREAL(A)
+%
+%   Input Arguments
+%     A - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also isnumeric, isa
     TF = true;
 end
