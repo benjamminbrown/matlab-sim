9efdde71ec7eb01fce83092dfd6522df7a6eaeab
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-07 20:29:23 -0400

    Update fix.m
    Add help documentation and comments throughout

--- +factors/@rationalFactors/fix.m
+++ +factors/@rationalFactors/fix.m
@@ -1,11 +1,28 @@
 function X = fix(X)
+% FIX - Round toward zero
+%   This function rounds each element of the input array to the nearest
+%   integer toward zero.
+%
+%   Syntax
+%     Y = FIX(X)
+%
+%   Input Arguments
+%     X - Input array
+%       scalar | vector | matrix | multidimensional array
+%
+%   See also ceil, floor, round
+
+    % Check for finite values
     isFinite = isfinite(X);
     if any(isFinite,"all")
-        isFiniteNegative = isFinite & X.Numerator.IsNegative~=X.Denominator.IsNegative;
+        % Fix the finite values to the nearest integer toward zero
         absN = uint64(abs(X.Numerator(isFinite)));
         absD = uint64(abs(X.Denominator(isFinite)));
+        % Find where the sign of the result will need to be flipped
+        isFiniteNegative = isFinite & X.Numerator.IsNegative~=X.Denominator.IsNegative;
         X(isFinite) = factors.rationalFactors((absN-mod(absN,absD))./absD);
         if any(isFiniteNegative,"all")
+            % Flip the sign of the result where appropriate
             X(isFiniteNegative) = -X(isFiniteNegative);
         end
     end
