faf7cc9b2c80aa2c4d34cdeaec7adb84cd15b250
Benjamin Brown <benjamminbrown@gmail.com>
2025-07-08 16:36:14 -0400

    Modify/add array manipulation methods
    Add ctranspose, remove unnecessary copies, add help documentation

--- +factors/@scaleFactors/permute.m
+++ +factors/@scaleFactors/permute.m
@@ -1,5 +1,18 @@
-function B = permute(A,dimorder)
-    B = A;
-    B.Factors = permute(A.Factors,dimorder);
-    B.Exponents = permute(B.Exponents,dimorder);
+function A = permute(A,dimorder)
+% PERMUTE - Permute array dimensions
+%   This function rearranges the dimensions of an array in the specified
+%   order.
+%
+%   Syntax
+%     B = PERMUTE(A,dimorder)
+%
+%   Input Arguments
+%     A - Input array
+%       scalar | vector | matrix | multidimensional array
+%     dimorder - Dimension order
+%       positive integer row vector
+%
+%   See also ipermute, reshape, shiftdim, transpose
+    A.Factors = permute(A.Factors,dimorder);
+    A.Exponents = permute(A.Exponents,dimorder);
 end
